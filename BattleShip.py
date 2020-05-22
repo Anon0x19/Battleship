eb0833b422c3cbacaf3906691558d4a6476c2524
Marc Perales <49318468+Anon0x19@users.noreply.github.com>
2020-05-22 12:36:07 +0200

    Update BattleShip.py

--- BattleShip.py
+++ BattleShip.py
@@ -1,42 +1,46 @@
-from random import randint  #imports the randint (random integer) from the random module
+from random import randint
 
 board = []
+guess_row = 0
+guess_col = 0
+ship_row = 0
+ship_col = 0
 
 for x in range(0, 5):
-  board.append(["O"] * 5)   #it appends "0" 5 times to board and repeats it 5 times
+  board.append(["O"] * 5)
 
-def print_board(board):   #defines the print_board function with the specific "board" parameter
+def print_board(board):
   for row in board:
-    print " ".join(row)   #it prints the 5 "0" in 5 different rows
+    print " ".join(row)
 
-print_board(board)  #runs the print_board function while using the board as parameter
+print_board(board)
 
 def random_row(board):
-  return randint(0, len(board) - 1)   #chooses a ranom row between 0 and the maximum value in the board and subtracts 1 and the first value in the list is 0 and randint wont choose 0
+  return randint(0, len(board) - 1)
 
 def random_col(board):
-  return randint(0, len(board[0]) - 1)  #chooses a ranom column between 0 and the maximum value in the board and subtracts 1 and the first value in the list is 0 and randint wont choose 0
+  return randint(0, len(board[0]) - 1)
 
-ship_row = random_row(board) #creates the variable ship_row and sets it to the random row
-ship_col = random_col(board) #creates a variable called ship_col and sets it to the ranom column
+ship_row = random_row(board)
+ship_col = random_col(board)
 
 for turn in range(4):
-  print "Turn", turn + 1    #repeats the code for all integers between 0 and 4, and adds a 1 to the turn
-  guess_row = int(raw_input("Guess Row: "))   #asks the user to input a row
-  guess_col = int(raw_input("Guess Col: "))   #asks the user to input a column
+  print "Turn", turn + 1
+  guess_row = int(raw_input("Guess Row: "))
+  guess_col = int(raw_input("Guess Col: "))
 
   if guess_row == ship_row and guess_col == ship_col:
-    print "Congratulations! You sank my battleship!"    #if you guess correctly both row and column, you "sink" the battle ship and complete the aim of the game
+    print "Congratulations! You sank my battleship!"
     break
   else:
     if guess_row not in range(5) or \
       guess_col not in range(5):
-      print "Oops, that's not even in the ocean."   #checks if the input is greater than 5 and if it is it prints the error message
+      print "Oops, that's not even in the ocean."
     elif board[guess_row][guess_col] == "X":
-      print( "You guessed that one already." )    #checks if the specific location has been replaced with an X and if it has, it returns the "error" message saying you have guessed that location already
+      print( "You guessed that one already." )
     else:
       print "You missed my battleship!"
-      board[guess_row][guess_col] = "X"     #replaces the 0 in the location you have specified with an X
+      board[guess_row][guess_col] = "X"
     if (turn == 3):
-      print "Game Over"   #if turns is exactly 3, it prints the message "Game Over"
+      print "Game Over"
     print_board(board)
